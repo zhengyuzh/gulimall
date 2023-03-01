e6a7ffab2ec577f62ae2317793f84d013fd8349a
18348375641 <a11083879A>
2023-03-01 21:09:34 +0800

    配置网关路由与路径重写，网关统一配置跨域。
    遇到的问题：1、需要修改人人网后台中的跨域，暂时屏蔽。跨域问题在网关中统一解决
                 2、前台调用后台方法，前缀要配置成后端网关的端口。通过整个端口进行映射

--- renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -15,12 +15,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .maxAge(3600);
-    }
+    /**
+     * 暂时屏蔽掉，在网关中设置了跨域，这里可省略，否则访问有问题
+     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("*")
+//            .allowCredentials(true)
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .maxAge(3600);
+//    }
 }
