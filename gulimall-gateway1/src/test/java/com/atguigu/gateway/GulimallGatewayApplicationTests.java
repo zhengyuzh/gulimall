c50f6c20998c8c522ab43d522f8f91bc5bfc27c5
18348375641 <a11083879A>
2023-03-01 09:18:48 +0800

    创建测试gateway API网关。 遇到的问题：需要统一gate和springboot的版本（2.1.3） 【2023-03-01】

---
+++ gulimall-gateway1/src/test/java/com/atguigu/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
