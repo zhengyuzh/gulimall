a7c211370fc7fe5922b0247474e2de52340d8a92
18348375641 <a11083879A>
2023-03-04 15:40:01 +0800

    测试上传文件到OSS服务器上存储
    大致流程
    - 1、开通阿里云对象存储服务器
    - 2、新建保存文件目录
    - 3、在代码中使用，需要开通子账号来进行访问
    【2023-03-04】

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,11 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.BrandService;
@@ -12,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -43,6 +50,34 @@ public class GulimallProductApplicationTests {
 //        Long[] catelogPath = categoryService.findCatelogPath(225L);
 //        log.info("完整路径：{}",Arrays.asList(catelogPath));
 //    }
+
+    @Test
+    public void testOss() throws FileNotFoundException {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-guangzhou.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tRbaeFt4UNGBSzAUtfk";
+        String accessKeySecret = "jcoKe3xZKOj4gH0FVDG5YY4mCTUnBb";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "examplebucket";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "exampledir/exampleobject.txt";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        //上传文件流
+        FileInputStream inputStream = new FileInputStream("F:\\Users\\lenovo\\Pictures\\联想截图\\联想截图_20221207142850.png");
+        ossClient.putObject("gulimall-zhengyu","联想截图_20221207142850.png",inputStream);
+
+        //关闭OSSCLient
+        ossClient.shutdown();
+        System.out.println("上传成功...");
+
+
+
+
+    }
 
 
     @Test
